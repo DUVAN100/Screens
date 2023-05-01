9e14e5a7c4e8bfac7b2502b94e74040dd4383e55
DUVAN100 <duvanvivas100@gmail.com>
2023-05-01 10:15:00 -0500

    Push Proyect

--- App.js
+++ App.js
@@ -1,30 +1,69 @@
-import { StyleSheet, Text, Button, View } from 'react-native';
-import * as React from 'react';
+import { StyleSheet, Text,  View } from 'react-native';
+import {useState} from 'react';
 import Contacts from './screens/Contacts'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { MaterialIcons } from '@expo/vector-icons'
+import { TextInput, Button, Icon } from 'react-native-paper';
+import { MaterialIcons } from '@expo/vector-icons';
+import { RegisterUser } from './screens/RegisterUser';
 
 
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+
+let users = [
+  {email:"duvan", name:"Duvan" ,password:"1", role:2},
+  {email:"duvan2", name:"Duvan2" ,password:"2", role:1}
+]
+
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator >
         <Stack.Screen name="MyTabs" component={MyTabs} options={{title:'Systems test'}}/>
-        {/* <Stack.Screen name='Products' component={ProductScreen} options={{title:'Productos'}}/>
-        <Stack.Screen name='Contacts' component={Contacts} options={{title:'Contactenos'}}/> */}
       </Stack.Navigator>
     </NavigationContainer>
   );  
 }
 function HomeScreen({navigation}){
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [findError, setFindError] = useState('');
   return(
     <View style={styles.container}>
       <Text style={{marginBottom:20}}>Log in</Text>
+      <TextInput
+          style={{margin:20}}
+          label="Email"
+          mode="outlined"
+          left={<TextInput.Icon icon="account" />}
+          onChangeText={email => setEmail(email)}
+          value={email}
+      />
+      <TextInput
+          style={{margin:20}}
+          label="Password"
+          mode="outlined"
+          right={<TextInput.Icon icon="eye" />}
+          onChangeText={password => setPassword(password)}
+          value={password}
+          secureTextEntry
+      />
+      <Button icon="login" mode="contained" onPress={() =>{
+        let findUser = users.find(usr => usr.email == email && usr.password == password)
+        if(findUser != undefined){
+          setFindError('')
+          const {name, email} = findUser
+          setEmail('')
+          setPassword('')
+          navigation.navigate('Contacts',{name:name, email:email} )
+        }else{
+          setFindError("Email y/o password invalid")
+        }
+      }}>Log in</Button>
+      <Text style={{color:'red'}}>{findError}</Text>
     </View>
 
   );
@@ -33,15 +72,21 @@ function MyTabs() {
   return (
     <Tab.Navigator
       screenOptions={{
-        headerShown:true,
+        headerShown:false,
         tabBarActiveTintColor:'red',
         tabBarInactiveTintColor:'gray',
         tabBarActiveBackgroundColor:'aqua',
-        tabBarInactiveBackgroundColor:'#6667' 
+        tabBarInactiveBackgroundColor:'#6667',
+        
       }} 
     >
       <Tab.Screen name="Home" component={HomeScreen} options={{
+        tabBarStyle:{display:'none'},
         tabBarIcon:()=> <MaterialIcons size={ 20 } name={'home'}  color={ 'red' }/>
+      }} />
+      <Tab.Screen name="User" component={RegisterUser} options={{
+        tabBarStyle:{display:'none'},
+        tabBarIcon:()=> <MaterialIcons size={ 20 } name={'Arrow Back'}  color={ 'red' }/>
       }} />
       <Tab.Screen name="Products" component={ProductScreen} options={{
         tabBarIcon:()=> <MaterialIcons size={ 20 } name={'apps'}  color={ 'red' }/>
--- screens/Contacts.jsx
+++ screens/Contacts.jsx
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, TextInput ,Button, View } from 'react-native';
 
 
-export default function Contacts(){
+export default function Contacts({navigation, route}){
+    const {name, email} = route.params;
     return(
         <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
             <Text style={{marginBottom:10}}>Contactenos</Text>
+            <Text style={{marginBottom:10}}>Your fullname is: {name} with email: {email}</Text>
         </View>
     )
 
